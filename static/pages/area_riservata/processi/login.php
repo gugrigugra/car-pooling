048556054137581f5e29bf0ad80f93ee2acc698f
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2024-03-23 11:42:30 +0100

    fatta pagina di login

--- static/pages/area_riservata/processi/login.php
+++ static/pages/area_riservata/processi/login.php
@@ -8,13 +8,15 @@ $nome = trim($_POST['nome']);
 $cognome = trim($_POST['cognome']);
 $password_cifrata = md5($_POST['password']);
 
-$select_nome_cognome = "SELECT nome, cognome, password FROM utente WHERE nome like ? AND cognome like ? AND password like ?";
-if ($stmt_nome_cognome = $db_connection->prepare($select_nome_cognome)) {
-    $stmt_nome_cognome->bind_param("sss", $nome, $cognome, $password_cifrata);
-    if ($stmt_nome_cognome->execute()) {
-        $result_nome_cognome = $stmt_nome_cognome->get_result();
-        if ($result_nome_cognome->num_rows > 0) {
+$select_nome = "SELECT nome FROM utente WHERE nome like ?";
+if ($stmt_nome = $db_connection->prepare($select_nome)) {
+    $stmt_nome->bind_param("s", $nome);
+    if ($stmt_nome->execute()) {
+        $result_nome = $stmt_nome->get_result();
+        if ($result_nome->num_rows > 0) {
             //accesso esseguito
+
+            $_SESSION['username'] = $nome;
             header("Location: ../../../../index.php");
             exit;
         } else {
@@ -25,5 +27,7 @@ if ($stmt_nome_cognome = $db_connection->prepare($select_nome_cognome)) {
         echo "Oops! Something went wrong. Please try again later.";
     }
 }
-$stmt_nome_cognome->close();
+
+
+$stmt_nome->close();
 $db_connection->close();
