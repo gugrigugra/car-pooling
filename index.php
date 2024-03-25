184b9d9e6e56d758def3830b8bea4a17774b2cc3
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2024-03-25 17:34:00 +0100

    eliminato file css

--- index.php
+++ index.php
@@ -10,7 +10,99 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Pooling home page</title>
-    <link rel="stylesheet" href="static/css/style.css">
+    <style>
+        /* Stile generale */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+
+        nav {
+            display: inline-block;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+            margin-left: 10px;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+
+        section {
+            margin-top: 25px;
+            margin-left: 25px;
+        }
+
+        /* Stile per il contenitore principale */
+        main {
+            margin-top: 25px !important;
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #f9f9f9;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+
+
+        /* Stile per le etichette */
+        label {
+            display: block;
+            margin-bottom: 6px;
+            font-weight: bold;
+        }
+
+        /* Stile per gli input */
+        input[type="text"],
+        input[type="date"],
+        input[type="number"],
+        input[type="submit"] {
+            width: calc(100% - 24px);
+            padding: 10px;
+            margin-bottom: 12px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            font-size: 16px;
+        }
+
+        /* Stile per il pulsante di invio */
+        input[type="submit"] {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+
+        /* Stile per il pulsante di invio al passaggio del mouse */
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+
+        /* Stile per messaggi di errore */
+        .error-message {
+            color: #ff0000;
+            margin-top: 8px;
+            font-size: 14px;
+        }
+    </style>
 </head>
 
 <body>
